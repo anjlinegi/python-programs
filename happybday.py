263a71e5ef7fce6670061213e6a25b37abf656d5
anjlinegi <anjalinei55555@gmail.com>
2020-04-08 01:04:47 +0530

    Add happybday.py file

---
+++ happybday.py
@@ -0,0 +1,7 @@
+def singHappyBday(name):
+	print(" happy bday to you!!"+"\n happy bday to you!!"+"\n happy bday to you dear"+name+"!!"+"\n happy bday to you!!")
+	
+name=input("enter name")
+singHappyBday(name)
+	
+	
