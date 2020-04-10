384dd5ace0ce56c4a80fa51baab52d7e0ffcee32
anjlinegi <anjalinei55555@gmail.com>
2020-04-11 01:16:25 +0530

    prints happy birthday in functioncall.py

---
+++ functioncall.py
@@ -0,0 +1,15 @@
+def happyBirthdayEmily():
+    print("Happy Birthday to you")
+    print("Happy Birthday to you")
+    print("Happy Birthday to you DEAr Emily!!")
+    print("Happy Birthday to you")
+def happyBirthdayAndrew():
+    print("Happy Birthday to you")
+    print("Happy Birthday to you")
+    print("Happy Birthday to you DEAr Andrew!!")
+    print("Happy Birthday to you")
+def main():
+    happyBirthdayEmily()
+    happyBirthdayAndrew()
+main()
+    
